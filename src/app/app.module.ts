09aff5213ca93ee16745fdb04c51b3e12618f1a7
KhaledGs5 <khaled.gassara@ensi-uma.tn>
2024-07-13 01:55:50 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+  xpos: number = 50;
+  ypos: number = 50;
+  step: number = 0.3;
+  greetinganime: boolean = false;
+  keyState: { [key: string]: boolean } = {}; 
+  scale: number = 1;
+
+  ngOnInit(): void {
+    this.updatePosition();
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    this.keyState[event.key] = true;
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent) {
+    this.keyState[event.key] = false;
+  }
+
+  updatePosition() {
+    if (this.keyState['z']) {
+      this.ypos = Math.max(this.ypos - this.step - 1, 0);
+    }
+    if (this.keyState['s']) {
+      this.ypos = Math.min(this.ypos + this.step + 1, 100);
+    }
+    if (this.keyState['q']) {
+      this.xpos = Math.max(this.xpos - this.step, 0);
+      this.scale = 1;
+    }
+    if (this.keyState['d']) {
+      this.xpos = Math.min(this.xpos + this.step, 100);
+      this.scale = -1;
+    }
+    console.log(this.scale)
+    this.greetinganime = this.keyState['q'] || this.keyState['d'];
+    window.requestAnimationFrame(() => this.updatePosition());
+  }
+}
