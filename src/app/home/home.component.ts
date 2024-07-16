65ac9edbd78e3a549282b99bd5694c44d2ae33bc
KhaledGs5 <khaled.gassara@ensi-uma.tn>
2024-07-16 17:25:03 +0100

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,15 +5,20 @@ import { Component, HostListener, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent implements OnInit {
   xpos: number = 50;
   ypos: number = 50;
   step: number = 0.3;
-  greetinganime: boolean = false;
+  walk: boolean = false;
   keyState: { [key: string]: boolean } = {}; 
   scale: number = 1;
   fly: boolean = false;
+  flynflame : boolean = false;
+  sprite: string = 'Idle.png';
+  characterWidth: number = 300; 
+  characterHeight: number = 300; 
+  size : number = 88;
+  flyTimeout : any;
 
   ngOnInit(): void {
     this.updatePosition();
@@ -44,8 +49,40 @@ export class HomeComponent implements OnInit {
       this.xpos = Math.min(this.xpos + this.step, 100);
       this.scale = -1;
     }
-    this.fly = this.keyState['z'] && (this.keyState['q'] || this.keyState['d']);
-    this.greetinganime = ((this.keyState['q'] || this.keyState['d']) && !(this.fly));
+
+    if (this.keyState['z'] && (this.keyState['q'] || this.keyState['d'])) {
+      this.fly = true; 
+      this.flyTimeout = setTimeout(() => {
+        this.fly = false; 
+        this.flynflame = true;
+      }, 200); 
+    }else{
+      clearTimeout(this.flyTimeout); 
+      this.fly = false; 
+      this.flynflame = false;
+    }
+    this.walk = !this.fly && (this.keyState['q'] || this.keyState['d']);
+    console.log(this.flynflame)
     window.requestAnimationFrame(() => this.updatePosition());
+  }
+
+  getAnimationClass() {
+    if (this.flynflame) {
+      this.sprite = 'Fly&FlameSeq.png';
+      this.size = 1440;
+      return 'FlynFlameAnimation';
+    }else if(this.fly){
+      this.sprite = 'FlySeq.png';
+      this.size = 900;
+      return 'FlyAnimation';
+    } else if (this.walk) {
+      this.sprite = 'WalkSeq.png';
+      this.size = 4000;
+      return 'WalkAnimation';
+    } else {
+      this.sprite = 'Idle.png';
+      this.size = 88;
+      return '';
+    }
   }
 }
